08176acfceda5aba1a4e65c71e967f38e3a82cfe
Yerupalle Vamsi <ymohanasuryavamsi47@gmail.com>
2026-09-17 16:41:55 +0530

    fix errorHandler.js hardcore

--- Day-5/src/middleware/errorHandler.js
+++ Day-5/src/middleware/errorHandler.js
@@ -1,13 +1,14 @@
 const AppError = require("../utils/AppError");
 
 function errorHandler(err, req, res, next) {
+
   console.error(err);
 
   if (err instanceof AppError) {
     return res.status(err.status).json({ error: err.message });
   }
 
-  res.status(500).json({ error: "Internal server error" });
+  res.status(500).json({ error: err.message });
 }
 
 module.exports = errorHandler;
